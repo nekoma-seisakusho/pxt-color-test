903d3b37a36bd3c3581fd64410e79863d63ffa9a
nekoma-seisakusho <nekoma.seisakusho@gmail.com>
2024-04-06 13:26:21 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,38 @@
+//% weight=100 color=#0080ff icon="\uf1b2"
+namespace カラーセンサー {
 
+    let R = 0
+    let G = 0
+    let B = 0
+
+
+    //% block="初期化"
+    export function init() {
+        pins.i2cWriteNumber(
+            42,
+            139,
+            NumberFormat.UInt16BE,
+            false
+        )
+        pins.i2cWriteNumber(
+            42,
+            11,
+            NumberFormat.UInt16BE,
+            false
+        )
+    }
+
+    //% block="RGB読み出し"
+    export function RGB_read() {
+        pins.i2cWriteNumber(
+            42,
+            3,
+            NumberFormat.UInt8BE,
+            true
+        )
+        R = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        G = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        B = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+
+    }
+}
