4e0b6890239a3964c557e0180e6c2c4f44b27be0
nekoma-seisakusho <48303566+nekoma-seisakusho@users.noreply.github.com>
2024-04-06 17:05:11 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -21,7 +21,7 @@ namespace カラーセンサー {
             false
         )
     }
-
+/*
     //% block="RGB読み出し"
     export function RGB_read():number{
         pins.i2cWriteNumber(
@@ -35,10 +35,38 @@ namespace カラーセンサー {
         B = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
         return R;
     }
-
-    //% block="R読み出し"
+*/
+    //% block="R"
     export function R_read(): number {
-    
+        pins.i2cWriteNumber(
+            42,
+            3,
+            NumberFormat.UInt8BE,
+            true
+        )
+        R = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
         return R;
+    }
+    //% block="G"
+    export function G_read(): number {
+        pins.i2cWriteNumber(
+            42,
+            5,
+            NumberFormat.UInt8BE,
+            true
+        )
+        G = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        return G;
+    }
+    //% block="B"
+    export function B_read(): number {
+        pins.i2cWriteNumber(
+            42,
+            7,
+            NumberFormat.UInt8BE,
+            true
+        )
+        B = pins.i2cReadNumber(42, NumberFormat.UInt16BE, true)
+        return B;
     }
 }
